d33b9f0aa2274c638eb5356772cf265ee51e28e4
HarshithaGurumurthy <146424156+HarshithaGurumurthy@users.noreply.github.com>
2025-02-18 14:13:21 +0530

    Create num.js

---
+++ Java_HandsOn/Javascript/num.js
@@ -0,0 +1,6 @@
+let num1= Number(prompt("Enter the first number: "));
+let num2= Number(prompt("Enter the second number: "));
+for (let i = 1; i <= num2; i++) 
+{
+    console.log(${num1} * ${i} = ${num1 * i});
+}
