d0a9fdc07d14a17419bbc73c5427a5295fd1789d
HarshithaGurumurthy <146424156+HarshithaGurumurthy@users.noreply.github.com>
2025-02-18 14:22:42 +0530

    Create multiplication.js

---
+++ Java_HandsOn/Javascript/multiplication.js
@@ -0,0 +1,7 @@
+function multiplicationTable() {
+    let number = 5;
+    let limit = 10;
+    for (let i = 1; i <= limit; i++) {
+        console.log(`${number} x ${i} = ${number * i}`);
+    }
+}
