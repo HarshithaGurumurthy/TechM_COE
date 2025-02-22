7faf845cab90eb053499cd5250589851a62202ee
HarshithaGurumurthy <146424156+HarshithaGurumurthy@users.noreply.github.com>
2025-02-22 15:11:40 +0530

    Add files via upload

--- Task_3_Java/Task_1/StudentRecords.java
+++ Task_3_Java/Task_1/StudentRecords.java
@@ -1,1 +1,44 @@
+import java.util.*;
 
+class Student {
+    String name;
+    int[] marks;
+    int total;
+    double average;
+
+    Student(String name, int[] marks) {
+        this.name = name;
+        this.marks = marks;
+        this.total = Arrays.stream(marks).sum();
+        this.average = (double) total / marks.length;
+    }
+}
+
+public class StudentRecords {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of students: ");
+        int n = sc.nextInt();
+        Student[] students = new Student[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter student name: ");
+            String name = sc.next();
+            System.out.print("Enter number of subjects: ");
+            int subjects = sc.nextInt();
+            int[] marks = new int[subjects];
+            for (int j = 0; j < subjects; j++) {
+                System.out.print("Enter marks for subject " + (j + 1) + ": ");
+                marks[j] = sc.nextInt();
+            }
+            students[i] = new Student(name, marks);
+        }
+
+        Arrays.sort(students, (a, b) -> Double.compare(b.average, a.average));
+
+        System.out.println("\nSorted Student List:");
+        for (Student s : students) {
+            System.out.println("Name: " + s.name + ", Total: " + s.total + ", Average: " + s.average);
+        }
+    }
+}
