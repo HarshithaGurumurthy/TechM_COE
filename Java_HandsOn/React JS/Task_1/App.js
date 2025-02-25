37e1f0acd2552a59add2439329cf082987660c9f
HarshithaGurumurthy <146424156+HarshithaGurumurthy@users.noreply.github.com>
2025-02-25 12:53:54 +0530

    Update App.js

--- Java_HandsOn/React JS/Task_1/App.js
+++ Java_HandsOn/React JS/Task_1/App.js
@@ -1,1 +1,23 @@
+import React, { useState } from 'react';
+
+function App() {
+  const [number, setNumber] = useState('');
+  const [result, setResult] = useState('');
+
+  const checkDivisibility = () => {
+    if (number === '') {
+      setResult('Please enter a number.');
+      return;
+    }
+
+    const num = Number(number);
+
+    if (isNaN(num)) {
+      setResult('Invalid input. Please enter a valid number.');
+    } else if (num % 13 === 0) {
+      setResult(`${num} is divisible by 13 ✅`);
+    } else {
+      setResult(`${num} is NOT divisible by 13 ❌`);
+    }
+  };
 
