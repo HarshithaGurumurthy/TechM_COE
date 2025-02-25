a56ee0cefe56c1b6cc14e4651a3d6d65bbb44f36
HarshithaGurumurthy <146424156+HarshithaGurumurthy@users.noreply.github.com>
2025-02-25 12:55:40 +0530

    Update index.js

--- Java_HandsOn/React JS/Task_1/index.js
+++ Java_HandsOn/React JS/Task_1/index.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+// import './index.css'; // Remove or comment this line
+
+const root = ReactDOM.createRoot(document.getElementById('root1'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
